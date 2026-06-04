2723d280a678fa5102fd188fc4f2eddbe7fe4eed
BaraaSalah <baraasalahbs96@gmail.com>
2026-06-03 21:49:05 -0400

    first commit

---
+++ AxsosAcademy/python/django/django_orm/users_templates/users_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+def index(request):
+    all_users = User.objects.all()
+    return render(request, 'index.html', {'users': all_users})
+
+def create_user(request):
+    if request.method == 'POST':
+        User.objects.create(
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            email_address=request.POST['email_address'],
+            age=request.POST['age']
+        )
+        return redirect('index')
